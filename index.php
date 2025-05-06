7c3273fba572e48670cd7b7f856ee29a818e0db7
Luka <luka.peric@nth.ch>
2025-05-06 19:04:29 +0200

    mcoric-php-test

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+$numbersJson = file_get_contents('numbers.json');
+$numbersArray = json_decode($numbersJson, true);
 ?>
 
 <!DOCTYPE html>
@@ -20,12 +22,38 @@
 <body>
   <main class="main">
     <!-- ovdje dodajte formu i tablicu -->
-
-    <!-- forma -->
-
+    <form action="numbersProcessor.php" method="post">
+        <label for="cijelibroj">Unesite broj:</label>
+        <input type="number" id="cijelibroj" name="cijelibroj" min="0" required>
+        <button type="submit">Posaljite</button>
+    </form>
 
     <!-- tablica -->
+    <table border="1">
+        <tr>
+            <th>Broj</th>
+            <th>Par/Nepar</th>
+            <th>Suma znamenki</th>
+            <th>Broj znamenki</th>
+        </tr>
 
+        <?php
+        foreach ($numbersArray as $number) {
+            $enteredNumber = $number['broj'];
+            $evenOrOdd = $number['parnost'];
+            $sum = $number['suma'];
+            $digits = $number['znamenke'];
+
+            echo "<tr>
+                    <td>$enteredNumber</td>
+                    <td>$evenOrOdd</td>
+                    <td>$sum</td>
+                    <td>$digits</td>
+                   </tr>";
+        }
+
+        ?>
+    </table>
 
   </main>
 </body>
