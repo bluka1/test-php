13d2b313471f2532ce90cc2a42783a95d86c68f6
igor <igor.jauk@gmail.com>
2025-05-06 19:15:12 +0200

    igorjauk

---
+++ funkcije.php
@@ -0,0 +1,45 @@
+<?php
+function cijeliBroj($broj)
+{
+    if (is_numeric($broj)) {
+        return floor($broj);
+    } else {
+        return "Nije broj";
+    }
+}
+function parIliNepar($broj)
+{
+    if (is_numeric($broj)) {
+        if ($broj % 2 == 0) {
+            return "Paran";
+        } else {
+            return "Neparan";
+        }
+    } else {
+        return "Nije broj";
+    }
+}
+
+function zbrojiSveZnamenke($broj)
+{
+    if (is_numeric($broj)) {
+        if (is_array($broj)) {
+            return array_sum($broj);
+        } else {
+            return "Nije niz";
+        }
+    } else {
+        return "Nije broj";
+    }
+}
+
+function izracunajBrojZnamenki($broj)
+{
+    if (is_numeric($broj)) {
+        $brojZnamenki = abs($broj);
+        return strlen((string) $brojZnamenki);
+    } else {
+        return "Nije broj";
+    }
+}
+?>
