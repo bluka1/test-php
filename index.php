20c822335752ceb3a8f167a596ac2a20f914e7dd
Luka Batarelo <luka.batarelo@thoughtcap.com>
2025-05-06 20:09:15 +0200

    bluka1 - rjesenje testa

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
 <?php
+  define('FILE_PATH', './numbers.json');
+  $json = file_get_contents(FILE_PATH);
+  $brojevi = json_decode($json, true);
 
+  function pretvoriBrojUString($broj) {
+    return (string) $broj;
+  }
+
+  function jeParan($broj) {
+    return $broj % 2 === 0 ? "paran" : "neparan";
+  }
+
+  function sumirajZnamenke($broj) {
+    $br = pretvoriBrojUString($broj);
+    $arr = str_split($br);
+    return array_sum($arr);
+  }
+
+  function prebrojiZnamenke($broj) {
+    $br = pretvoriBrojUString($broj);
+    return strlen($br);
+  }
+
+  if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["broj"])) {
+    $input = trim($_POST["broj"]);
+
+    $input = (int)$input;
+    $noviBroj = [
+      "broj" => $input,
+      "parnost" => jeParan($input),
+      "suma" => sumirajZnamenke($input),
+      "znamenke" => prebrojiZnamenke($input)
+    ];
+    $brojevi[] = $noviBroj;
+    file_put_contents(FILE_PATH, json_encode($brojevi, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+  }
 ?>
 
 <!DOCTYPE html>
@@ -22,11 +57,34 @@
     <!-- ovdje dodajte formu i tablicu -->
 
     <!-- forma -->
-
+    <form action="" method="post">
+      <label>
+        Upišite broj:
+        <br>
+        <input type="number" name="broj" required>
+      </label>
+      <br><br>
+      <!-- <button type="submit">Pošalji</button> -->
+      <input type="submit" value="Pošalji">
+    </form>
 
     <!-- tablica -->
-
-
+    <table border="1">
+      <tr>
+        <th>Broj</th>
+        <th>Par/Nepar</th>
+        <th>Suma znamenki</th>
+        <th>Broj znamenki</th>
+      </tr>
+      <?php foreach ($brojevi as $broj): ?>
+        <tr>
+          <td><?php echo $broj['broj'] ?></td>
+          <td><?php echo $broj['parnost'] ?></td>
+          <td><?php echo $broj['suma'] ?></td>
+          <td><?php echo $broj['znamenke'] ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
   </main>
 </body>
 </html>
