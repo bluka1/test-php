9ed4b0ab0f5d36a8f768e4f0b3ca37b0c47be1a4
Luka Batarelo <luka.batarelo@thoughtcap.com>
2025-05-06 18:07:11 +0200

    main - zadatak

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>test php</title>
+
+  <style>
+    .main {
+      display: flex;
+      justify-content: space-evenly;
+    }
+  </style>
+  
+</head>
+<body>
+  <main class="main">
+    <!-- ovdje dodajte formu i tablicu -->
+
+    <!-- forma -->
+
+
+    <!-- tablica -->
+
+
+  </main>
+</body>
+</html>
